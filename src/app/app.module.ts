a3e3eab633466680173b4fe6b7ea902c888beb44
AndroGenium <chilachava.doc@gmail.com>
2024-06-09 19:31:35 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+import { TextComponent } from './components/navbar/navbar/text/text.component';
+import { FormsModule, NgForm } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TextComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/navbar/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  value: any;
+}
